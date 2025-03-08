d124a1ad2f03af8df9b06cf534d7f77bdf0208da
Cristiano Silveira <142178660+Cris-noscore@users.noreply.github.com>
2025-03-08 19:28:35 +0000

    commit5

--- prj/src/presentation/routes/useRoutes.ts
+++ prj/src/presentation/routes/useRoutes.ts
@@ -2,6 +2,6 @@ import {Router} from "express"
 
 const router = Router()
 
-router.post("/user", (req, res) => {res.send("user rotes")})
+router.get("/user", (req, res) => {res.send("user rotes")})
 
 export default router
