eee47b73710502b47cbe16b35c1ebaf706484204
Cristiano Silveira <142178660+Cris-noscore@users.noreply.github.com>
2025-03-08 19:25:25 +0000

    commit4

--- prj/src/index.ts
+++ prj/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express"
-//import userRoutes from ""
+import userRoutes from "./presentation/routes/useRoutes"
 import dotenv from "dotenv"
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use("/api", userRoutes)
 
 const PORT = process.env.PORT || 3000
 
--- prj/src/presentation/routes/useRoutes.ts
+++ prj/src/presentation/routes/useRoutes.ts
@@ -2,6 +2,6 @@ import {Router} from "express"
 
 const router = Router()
 
-router.post("/", (req, res) => {res.send("user rotes")})
+router.post("/user", (req, res) => {res.send("user rotes")})
 
 export default router
