f1cb378d445df01afea96097fbbfb9278224cf00
Cristiano Silveira <142178660+Cris-noscore@users.noreply.github.com>
2025-03-08 19:09:50 +0000

    commit3

---
+++ prj/src/presentation/routes/useRoutes.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+router.post("/", (req, res) => { res.send("user rotes"); });
+exports.default = router;
