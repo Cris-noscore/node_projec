ebfa4cf580c5703084075f15efb7561ae9cca8b5
Cristiano Silveira <142178660+Cris-noscore@users.noreply.github.com>
2025-03-15 15:58:26 -0300

    commit 11

--- prj/infrastructure/repositories/UserRepository.ts
+++ prj/infrastructure/repositories/UserRepository.ts
@@ -3,5 +3,9 @@ import { User } from "@domain/entities/User"
 import { Database } from "infrastructure/config/Database"
 
 export class UserRepository implements IUserRepository{
+    private pool = Database.getConnection()
+    async save(user: User) Promise<User> {
+        return user 
+    }
 
 }
