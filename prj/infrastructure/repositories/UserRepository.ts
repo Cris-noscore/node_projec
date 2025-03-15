d449e2f268eaa1c9a0610538abed89a60aa93452
Cristiano Silveira <142178660+Cris-noscore@users.noreply.github.com>
2025-03-15 15:53:46 -0300

    commit 10

---
+++ prj/infrastructure/repositories/UserRepository.ts
@@ -0,0 +1,7 @@
+import { UserRepository } from "@domain/repositories/UserRepository"
+import { User } from "@domain/entities/User"
+import { Database } from "infrastructure/config/Database"
+
+export class UserRepository implements IUserRepository{
+
+}
