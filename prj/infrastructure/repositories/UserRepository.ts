00c2b7abce9a0b93b0da75c1dfd1f91488da3262
Cristiano Silveira <142178660+Cris-noscore@users.noreply.github.com>
2025-03-15 16:10:08 -0300

    commit 12

--- prj/infrastructure/repositories/UserRepository.ts
+++ prj/infrastructure/repositories/UserRepository.ts
@@ -5,6 +5,11 @@ import { Database } from "infrastructure/config/Database"
 export class UserRepository implements IUserRepository{
     private pool = Database.getConnection()
     async save(user: User) Promise<User> {
+        const [result] = await this.pool.execute(
+            "INSERT into users (name, email) VALUES(?, ?)", [user.name, user.email]
+        )
+        const insert Id = (result as any).insertId
+        UserRepository.id = insertId
         return user 
     }
 
